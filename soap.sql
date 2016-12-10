f3f1ba4b107bf2df79f7bb9cae4109d699f8714f
JamesAlfano <jalfano1@umbc.edu>
2016-12-09 20:07:41 -0500

    Add files via upload

--- soap.sql
+++ soap.sql
@@ -1,10 +1,8 @@
-create table office
+create table Office
        (office_name		varchar(25),
        	office_city		varchar(25),
 	office_sqft		numeric(10,0) check (office_sqft > 0),
-	primary key (office_name),
-	foreign key (office_name) references Manages
-		on delete cascade
+	primary key (office_name)
 	);
 
 create table CustomerAgencies
@@ -13,25 +11,21 @@ create table CustomerAgencies
 	agency_city		varchar(25),
 	phonenumber		varchar(10),
 	primary key (agency_id),
-	foreign key (agency_id) references Manages
+	foreign key (agency_city) references AgencyLocation
 		on delete cascade
 	);
 
 create table AgencyLocation
        (agency_city		varchar(25),
        	agency_address		varchar(100),
-	primary key (agency_city),
-	foreign key (agency_city) references CustomerAgencies (agency_city)
-		on delete set null
+	primary key (agency_city)
 	);
 
 create table RentalAgreement
        (rental_id		varchar(8),
         rentamount		numeric(10,0) check (rentamount > 0),
 	enddate			varchar(8),
-	primary key (rental_id),
-	foreign key (rental_id) references Manage
-		on delete cascade
+	primary key (rental_id)
 	);
 
 create table Manage
@@ -39,5 +33,8 @@ create table Manage
         office_name		varchar(25),
 	agency_id		varchar(8),
 	rental_id		varchar(8),
-	primary key (manage_id)
+	primary key (manage_id, office_name, agency_id, rental_id),
+	foreign key (office_name) references Office,
+	foreign key (agency_id) references CustomerAgencies,
+	foreign key (rental_id) references RentalAgreement
 	);
